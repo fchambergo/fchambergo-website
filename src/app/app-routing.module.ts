a046b905917db44e84df3aa4949f5f566b7f823e
Frank Chambergo <franky4908@gmail.com>
2021-01-16 18:02:03 -0500

    router links for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { JourneyComponent } from './journey/journey.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { WorkComponent } from './work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'journey', component: JourneyComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
