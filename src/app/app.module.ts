5233dc3c1346b9fea1145f3183d5d7358958c476
Frank Chambergo <franky4908@gmail.com>
2021-01-18 19:57:12 -0500

    resume redesign with navbar included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { WorkComponent } from './work/work.component';
 import { JourneyComponent } from './journey/journey.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ResumeComponent } from './resume/resume.component';
+import { ExperienceComponent } from './resume/experience/experience.component';
+import { SkillsComponent } from './resume/skills/skills.component';
+import { EducationComponent } from './resume/education/education.component';
+import { SummaryComponent } from './resume/summary/summary.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { ResumeComponent } from './resume/resume.component';
     WorkComponent,
     JourneyComponent,
     NavbarComponent,
-    ResumeComponent
+    ResumeComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    EducationComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
