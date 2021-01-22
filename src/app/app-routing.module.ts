04619b54dc05bbc46ea45259c56d8f058d5cf15b
Frank Chambergo <franky4908@gmail.com>
2021-01-22 17:52:48 -0500

    resume education component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { JourneyComponent } from './journey/journey.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { EducationComponent } from './resume/education/education.component';
+import { ExperienceComponent } from './resume/experience/experience.component';
 import { ResumeComponent } from './resume/resume.component';
+import { SkillsComponent } from './resume/skills/skills.component';
 import { WorkComponent } from './work/work.component';
 
 const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
-  { path: 'resume', component: ResumeComponent },
+  { path: 'resume', component: ResumeComponent, 
+    children: [
+      { path: 'education', component: EducationComponent },
+      { path: 'experience', component: ExperienceComponent },
+      { path: 'skills', component: SkillsComponent },
+      { path: '', component: AboutComponent },
+      { path: 'about', component: AboutComponent }
+    ]
+  },
   { path: 'work', component: WorkComponent },
   { path: 'journey', component: JourneyComponent }
 
