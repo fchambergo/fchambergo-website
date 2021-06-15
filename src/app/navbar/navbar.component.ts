7e69ccd70b88ab28352539822b81d50cea790980
Frank Chambergo <franky4908@gmail.com>
2021-06-14 21:49:00 -0400

    navbar dropdown cover close functionality for links
    closes dropdown cover when dropdown menu options are clicked

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,6 +10,17 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.addNavbarCloseFunctionality();
+  }
+
+  closeCoverNavbar() {
+    var coverNavbar = document.getElementsByClassName('menu-dropdown-cover');
+    var hamburger = document.getElementsByClassName('menu-toggle')[0];
+    if (coverNavbar.length > 0) {
+      const el = document.querySelector('.navbar__items');
+      el.classList.remove('menu-dropdown-cover');
+      hamburger.classList.remove('is-active');
+    }
   }
 
   toggleNav() {
@@ -18,6 +29,14 @@ export class NavbarComponent implements OnInit {
 
     var menu = document.getElementsByClassName('navbar__items')[0];
     menu.classList.toggle('menu-dropdown-cover');
+  }
+
+  addNavbarCloseFunctionality() {
+    var links = document.getElementsByClassName('navbar__item');
+    
+    for(var i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', this.closeCoverNavbar)
+    }
   }
 
 }
