c4c91ee6096204150bcd8e90f1409df6ecaa38ec
Frank Chambergo <franky4908@gmail.com>
2021-06-21 19:36:09 -0400

    formed contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { JourneyComponent } from './journey/journey.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ResumeComponent } from './resume/resume.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'work', component: WorkComponent },
   { path: 'journey', component: JourneyComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', component: LandingPageComponent }
 
 ];
