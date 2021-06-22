54827889b66eaf316e6af7185110fd3ed84207a1
Frank Chambergo <franky4908@gmail.com>
2021-06-22 00:08:14 -0400

    resume page deleted.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,10 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { JourneyComponent } from './journey/journey.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { ResumeComponent } from './resume/resume.component';
 import { WorkComponent } from './work/work.component';
 
 const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
-  { path: 'resume', component: ResumeComponent },
   { path: 'work', component: WorkComponent },
   { path: 'journey', component: JourneyComponent },
   { path: 'about', component: AboutComponent },
